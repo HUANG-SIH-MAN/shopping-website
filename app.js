a7fc267529c476b98cb82fd4f8fcf3be1f57b9c0
HUANG-SIH-MAN <cindy20303705@gmail.com>
2022-01-04 11:27:54 +0800

    feat:建立專案基本架構(使用express框架+handlebars樣板+MySQL資料庫)

---
+++ app.js
@@ -0,0 +1,20 @@
+//express框架
+const express = require('express')
+const app = express()
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+const port = process.env.port
+
+//handlebars樣板設定
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+//路由設定
+const routes = require('./routers')
+
+app.use(routes)
+app.listen(port ,()=>{
+    console.log(`localhost:${port}`)
+})
---
+++ routers/index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+const home = require('./modules/home')
+
+router.use('/', home)
+module.exports = router
