1ad070adbe04e4a42ef6da192f2c1f930f108128
HUANG-SIH-MAN <cindy20303705@gmail.com>
2022-01-07 11:07:55 +0800

    feat:新增註冊、登入頁面

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
+const user = require('./modules/user')
 
+router.use('/users', user)
 router.use('/', home)
 module.exports = router
---
+++ routers/modules/user.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const userController = require('../../controller/userController')
+
+//註冊、登入、登出
+router.get('/register', userController.registerPage)
+router.get('/login', userController.loginPage)
+
+module.exports = router
