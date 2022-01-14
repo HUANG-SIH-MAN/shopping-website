9c82755a02ae54756e32be236f217fcd45838279
HUANG-SIH-MAN <cindy20303705@gmail.com>
2022-01-15 00:07:53 +0800

    feat:購物車可調整購買商品數量、或刪除商品

--- controller/cartController.js
+++ controller/cartController.js
@@ -1,7 +1,19 @@
 const db = require('../models')
 const Cart = db.Cart
+const Commodity = db.Commodity
 
 const cartController = {
+    cartListPage: (req, res) => {
+        Cart.findAll({
+            raw: true, nest: true,
+            where: { userId: req.user.id},
+            include: [ Commodity ]
+        })
+        .then(cart => {
+            const totalAmount = cart.map(i => i.quantity * i.Commodity.price).reduce((a,b)=> a + b)
+            return res.render('cart', { cart, totalAmount })
+        })
+    },
     addCommodity: (req, res) => {
         Cart.findOrCreate({
             where: { userId: req.user.id, commodityId: req.params.commodityId }, 
@@ -12,6 +24,33 @@ const cartController = {
         })
         .then(()=> {
             req.flash('success', '成功將商品加入購物車!!')
+            return res.redirect('back')
+        })
+    },
+    removeCommodity: (req, res) => {
+        Cart.destroy({ 
+            where: { id: req.params.id } 
+        })
+        .then(() => {
+            req.flash('success', '成功將商品移除!!')
+            return res.redirect('back')
+        })
+    },
+    addQuantity: (req, res) => {
+        Cart.findByPk(req.params.id)
+        .then(cart => {
+            cart.increment({quantity: 1})
+            return res.redirect('back')
+        })
+    },
+    reduceQuantity: (req, res) => {
+        Cart.findByPk(req.params.id)
+        .then(cart => {
+            if (cart.dataValues.quantity <= 1) {
+                req.flash('error', '商品數量至少為1')
+            } else {
+                cart.decrement({quantity: 1}) 
+            }
             return res.redirect('back')
         })
     }
--- routers/modules/cart.js
+++ routers/modules/cart.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const cartController = require('../../controller/cartController')
 
+router.get('/', cartController.cartListPage)
+router.put('/addQuantity/:id', cartController.addQuantity)
+router.put('/reduceQuantity/:id', cartController.reduceQuantity)
 router.post('/:commodityId', cartController.addCommodity)
+router.delete('/:id', cartController.removeCommodity)
 
 module.exports = router
